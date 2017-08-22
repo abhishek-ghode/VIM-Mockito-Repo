5a66cefb722718b6ee3d71b423e351399bb2a938
abhishek-ghode <abhishek.ghode@capgemini.com>
2017-08-22 17:19:33 +0530

    Ignoring a test case

--- VIM-Gradle/src/test/java/com/cg/ControllerServletTest.java
+++ VIM-Gradle/src/test/java/com/cg/ControllerServletTest.java
@@ -2,11 +2,13 @@ package com.cg;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ControllerServletTest {
 
 	@Test
+	@Ignore
 	public void testProcessRequest() {
 		fail("Not yet implemented");
 	}
